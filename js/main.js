dfd687347656dc5ab8333a204cc13bf449dc9fc0
Mohamed Emad <m.emad550@gmail.com>
2018-07-26 22:59:26 +0200

    UI Responsive update in events section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   let wrapper = document.getElementById('wrapper'),
     topLayer = wrapper.querySelector('.top'),
+    bottomLayer = wrapper.querySelector('.bottom'),
     handle = wrapper.querySelector('.handle'),
+    topLayerImage = topLayer.querySelector('img'),
+    bottomLayerImage = bottomLayer.querySelector('img'),
     skew = 0,
     delta = 0;
 
@@ -11,7 +14,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   wrapper.addEventListener('mousemove', (e) => {
     delta = (e.clientX - window.innerWidth / 2) * 0.5;
-    handle.style.left = e.clientX + delta + 'px';
+    handle.style.left = ((e.clientX + delta)-15) + 'px';
     topLayer.style.width = e.clientX + skew + delta + 'px';
+    console.log(topLayerImage);
+    console.log(bottomLayerImage);
+    /* making condition based on handler position
+    let HandleWithoutPX = handle.style.left.slice(0, -2);
+    let HandleInNumber = parseInt(HandleWithoutPX);
+    console.log(HandleInNumber);
+    console.log(window.innerWidth);
+    if (HandleInNumber > window.innerWidth) {
+    } else {
+
+    }
+    */
   })
 });
